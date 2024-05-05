bccffeb7fc3b2a671dc8be884cfede01bc98f6a1
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-05-05 18:49:45 +0530

    Update git.py

--- AnieXEricaMusic/core/git.py
+++ AnieXEricaMusic/core/git.py
@@ -28,17 +28,7 @@ def install_req(cmd: str) -> Tuple[str, str, int, int]:
 
     return asyncio.get_event_loop().run_until_complete(install_requirements())
 
-
-def git():
-    REPO_LINK = config.UPSTREAM_REPO
-    if config.GIT_TOKEN:
-        GIT_USERNAME = REPO_LINK.split("com/")[1].split("/")[0]
-        TEMP_REPO = REPO_LINK.split("https://")[1]
-        UPSTREAM_REPO = f"https://{GIT_USERNAME}:{config.GIT_TOKEN}@{TEMP_REPO}"
-    else:
-        UPSTREAM_REPO = config.UPSTREAM_REPO
-    try:
-        repo = Repo()
+    
         LOGGER(__name__).info(f"Git Client Found [VPS DEPLOYER]")
     except GitCommandError:
         LOGGER(__name__).info(f"Invalid Git Command")
